1a8def8c263c78e757a4cdf4deb801e4841e0d23
stephensalah <stephen@salahfamily.org>
2022-04-28 15:00:47 -0500

    Need to fix a bug

--- BrickBasher-Unity/Assets/Scripts/Ball.cs
+++ BrickBasher-Unity/Assets/Scripts/Ball.cs
@@ -17,15 +17,31 @@ using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
-    //[Header("General Settings")]
+    
+    
+
+    [Header("General Settings")]
+
+    public float score;
+    public GameObject paddle;
+    public AudioClip sound;
+    private AudioSource audioSource;
+    public Text scoreTxt;
+    public Text ballTxt;
+
+
 
     
 
 
 
-    //[Header("Ball Settings")]
+    [Header("Ball Settings")]
     
-    
+    public float numBalls;
+    public float speed;
+    public Vector3 initialForce;
+    public bool isInPlay=false;
+    private Rigidbody rb;
 
    
 
@@ -36,6 +52,8 @@ public class Ball : MonoBehaviour
     //Awake is called when the game loads (before Start).  Awake only once during the lifetime of the script instance.
     void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        audioSource=GetComponent<AudioSource>();
 
     }//end Awake()
 
@@ -43,7 +61,7 @@ public class Ball : MonoBehaviour
         // Start is called before the first frame update
         void Start()
     {
-        //SetStartingPos(); //set the starting position
+        SetStartingPos(); //set the starting position
 
     }//end Start()
 
@@ -52,16 +70,57 @@ public class Ball : MonoBehaviour
     void Update()
     {
         
+        if (isInPlay==false){
+            Vector3 pos=this.transform.position;
+            pos.x=paddle.transform.position.x;
+            this.transform.position=pos;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) ){
+            if (isInPlay==false){
+                isInPlay=true;
+                Move();
+            }
+
+        }
+
+        
     }//end Update()
 
 
     private void LateUpdate()
     {
+        if (isInPlay==true){
+            
+            initialForce.y=speed;
+        }
 
 
     }//end LateUpdate()
+    void Move(){
+        rb.AddForce(initialForce);
+        Debug.Log(initialForce);
+        
+    }
+    private void OnCollisionEnter(Collision collision){
+        GameObject GO=collision.gameObject;
+        if (GO.tag=="Brick"){
+            score+=100;
+            Destroy(GO);
+        }
+    }
+    private void OnTriggerEnter(Collider other){
+        
+        if (other.tag=="OutBounds"){
+            numBalls-=1;
+        }
+        if (numBalls>0){
+            Invoke("SetStartingPos",2);
+        }
+    }
 
-    /*
+    
+
+    
     void SetStartingPos()
     {
         isInPlay = false;//ball is not in play
@@ -74,7 +133,7 @@ public class Ball : MonoBehaviour
         transform.position = pos;//set starting position of the ball 
     }//end SetStartingPos()
 
-    */
+    
 
 
 
